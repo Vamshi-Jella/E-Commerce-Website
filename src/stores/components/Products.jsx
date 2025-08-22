68cc8fd94b8aea261d97d0f99819dda5041246bd
Vamshi-Jella <vamshijella21@gmail.com>
2025-08-22 20:18:08 +0530

    updated components

--- src/stores/components/Products.jsx
+++ src/stores/components/Products.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Mobiles from "./Mobiles";
+import Computers from "./Computers";
+import Watch from "./Watch";
+import Men from "./Men";
+import Women from "./Women";
+import Furniture from "./Furniture";
+import AC from "./AC";
+import Kitchen from "./Kitchen";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <Mobiles />
+      <Computers />
+      <Watch />
+      <Men />
+      <Women />
+      <Furniture />
+      <AC />
+      <Kitchen />
+    </div>
+  );
+};
 
-export default Products
+export default Products;
